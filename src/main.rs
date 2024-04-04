a423f4459fef0219e76692554434920ee272dd0e
DroneBreaker <brownierz01@gmail.com>
2024-04-04 18:45:44 +0100

    Added mods to main

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
-use actix_web::{self, App, HttpServer};
-use user::routes::all_users;
+use actix_web::{self, get, App, HttpResponse, HttpServer, Responder};
+use user::user_routes::all_users;
 
 mod db;
 mod user;
 
+#[get("/")]
+pub async fn login() -> impl Responder {
+    HttpResponse::Ok().body("Welcome to Apex Dashboard")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
-        App::new().service(all_users)
+        App::new()
+        .service(login)
+        .service(all_users)
     })
     .bind("127.0.0.1:8080")?
     .run()
